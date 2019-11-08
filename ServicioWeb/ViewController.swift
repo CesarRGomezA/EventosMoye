7013dc2759ad1a95910b6d4428d77f6b68a6f903
Cesar Gomez <cesargomez@MacBook-Pro-de-lirenia.local>
2019-11-08 13:21:48 -0700

    1er commit

---
+++ ServicioWeb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ServicioWeb
+//
+//  Created by Cesar Gomez on 10/29/19.
+//  Copyright © 2019 Cesar Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
